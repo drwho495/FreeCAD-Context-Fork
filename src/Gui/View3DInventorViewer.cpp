195a0d898d767709591aaccb241cc3d16880c9be
Zheng, Lei <realthunder.dev@gmail.com>
2021-10-28 17:59:12 +0800

    Gui: show on top icon in tree view
    Implement auto sync on-top objects due to hierarchy changes in tree view
    
    Fixes realthunder/FreeCAD_Assembly3#881

--- src/Gui/View3DInventorViewer.cpp
+++ src/Gui/View3DInventorViewer.cpp
@@ -1031,7 +1031,6 @@ void View3DInventorViewer::clearGroupOnTop(bool alt) {
     if (alt && guiDocument && _pimpl->objectsOnTop.size()) {
         _pimpl->objectsOnTop.clear();
         guiDocument->signalOnTopObject(SelectionChanges::ClrSelection, App::SubObjectT());
-    }
 
     if(objectsOnTopSel.empty() && objectsOnTopPreSel.empty())
         return;
@@ -1131,7 +1130,6 @@ bool View3DInventorViewer::isInGroupOnTop(const std::string &key, bool altOnly) 
 }
 
 void View3DInventorViewer::checkGroupOnTop(const SelectionChanges &Reason, bool alt) {
-
     bool preselect = false;
 
     switch(Reason.Type) {
@@ -1361,6 +1359,23 @@ void View3DInventorViewer::checkGroupOnTop(const SelectionChanges &Reason, bool 
 
     SoTempPath path(10);
     path.ref();
+
+    if (alt && guiDocument) {
+        switch(Reason.Type) {
+        case SelectionChanges::AddSelection:
+        case SelectionChanges::RmvSelection: {
+            auto objT = Reason.Object;
+            objT.setSubName(objT.getSubNameNoElement());
+            if (Reason.Type == SelectionChanges::AddSelection)
+                _pimpl->objectsOnTop.insert(objT);
+            else
+                _pimpl->objectsOnTop.erase(objT);
+            guiDocument->signalOnTopObject(Reason.Type, objT);
+            break;
+        } default:
+            break;
+        }
+    }
 
     if (alt && guiDocument) {
         switch(Reason.Type) {
