ba9d24df724eeade3ef50afe6108d175d9b60e5c
Zheng, Lei <realthunder.dev@gmail.com>
2022-06-12 08:22:38 +0800

    Gui: fix overlay tooltip

--- src/Gui/Widgets.cpp
+++ src/Gui/Widgets.cpp
@@ -1148,10 +1148,10 @@ TipLabel *TipLabel::instance(QWidget *parent, const QPoint &pos, bool overlay)
     return _OverlayTipLabel;
 }
 
-void TipLabel::hideLabel()
+void TipLabel::hideLabel(bool hideOverlay)
 {
     auto doHide = [](TipLabel *label) {
-        if (label) {
+        if (label && label->isVisible()) {
             label->setVisible(false);
             if (label->movie()) {
                 label->movie()->stop();
@@ -1160,7 +1160,8 @@ void TipLabel::hideLabel()
         }
     };
     doHide(_TipLabel);
-    doHide(_OverlayTipLabel);
+    if (hideOverlay)
+        doHide(_OverlayTipLabel);
 }
 
 void TipLabel::refreshIcons()
@@ -1290,8 +1291,10 @@ ToolTip::ToolTip() : hidden(true)
 {
     tooltipTimer.setSingleShot(true);
     hideTimer.setSingleShot(true);
+    hideTimerNoOverlay.setSingleShot(true);
     connect(&tooltipTimer, &QTimer::timeout, [this]() {onShowTimer();});
-    connect(&hideTimer, &QTimer::timeout, [this]() {onHideTimer();});
+    connect(&hideTimer, &QTimer::timeout, [this]() {onHideTimer(true);});
+    connect(&hideTimerNoOverlay, &QTimer::timeout, [this]() {onHideTimer(false);});
     qApp->installEventFilter(this);
 }
 
@@ -1424,11 +1427,11 @@ void ToolTip::onShowTimer()
     displayTime.restart();
 }
 
-void ToolTip::onHideTimer()
+void ToolTip::onHideTimer(bool hideOverlay)
 {
     hideTimer.stop();
     tooltipTimer.stop();
-    TipLabel::hideLabel();
+    TipLabel::hideLabel(hideOverlay);
 }
 
 bool ToolTip::checkToolTip(QWidget *w, QHelpEvent *helpEvent) {
@@ -1450,7 +1453,7 @@ bool ToolTip::checkToolTip(QWidget *w, QHelpEvent *helpEvent) {
         tooltip = w->toolTip();
 
     if (tooltip.isEmpty()) {
-        ToolTip::hideText(100);
+        instance()->hideTimerNoOverlay.start(300);
         return false;
     }
 
--- src/Gui/Widgets.h
+++ src/Gui/Widgets.h
@@ -397,7 +397,7 @@ class GuiExport TipLabel: public QLabel
 public:
     TipLabel(QWidget *parent, Qt::WindowFlags flags = Qt::WindowFlags());
     static TipLabel *instance(QWidget *parent, const QPoint &pos, bool overlay);
-    static void hideLabel();
+    static void hideLabel(bool hideOverlay=true);
     static void refreshIcons();
     void set(const QString &, const QString &iconPath = QString());
 protected:
@@ -445,7 +445,7 @@ protected:
     virtual ~ToolTip();
 
     void onShowTimer();
-    void onHideTimer();
+    void onHideTimer(bool hideOverlay);
     bool eventFilter(QObject* o, QEvent*e);
     static bool checkToolTip(QWidget *w, QHelpEvent *e);
 
@@ -460,6 +460,7 @@ private:
     QPointer<QWidget> w; // need guard in case widget gets destroyed
     QTimer tooltipTimer;
     QTimer hideTimer;
+    QTimer hideTimerNoOverlay;
     QElapsedTimer displayTime;
 };
 
