546808824382d5a9524f23c41bf5a5fd3aa797b9
Zheng, Lei <realthunder.dev@gmail.com>
2022-06-13 13:07:21 +0800

    Spreadsheet: fix crash on undo/redo

--- src/Mod/Spreadsheet/App/Sheet.cpp
+++ src/Mod/Spreadsheet/App/Sheet.cpp
@@ -846,7 +846,7 @@ const char* Sheet::getPropertyName(const App::Property* prop) const
                 return iter->second.c_str();
         }
     }
-    return prop->getName();
+    return App::DocumentObject::getPropertyName(prop);
 }
 
 void Sheet::touchCells(Range range) {
