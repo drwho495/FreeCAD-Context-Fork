4eb9df973306723de54ab82bd0aa1e50d8817a3b
Zheng, Lei <realthunder.dev@gmail.com>
2022-06-24 10:23:48 +0800

    PartDesign: suppress tag tooltip when drag and drop

--- src/Mod/PartDesign/Gui/ViewProvider.cpp
+++ src/Mod/PartDesign/Gui/ViewProvider.cpp
@@ -42,6 +42,7 @@
 #include <Gui/SoFCUnifiedSelection.h>
 #include <Gui/CommandT.h>
 #include <Gui/ViewParams.h>
+#include <Gui/Tree.h>
 #include <Base/Exception.h>
 #include <Base/Tools.h>
 #include <Mod/Part/App/PartParams.h>
@@ -690,14 +691,16 @@ bool ViewProvider::iconMouseEvent(QMouseEvent *ev, const QByteArray &tag)
 
 QString ViewProvider::getToolTip(const QByteArray &tag) const
 {
-    if (tag == _SuppressedTag)
-        return QObject::tr("Feature suppressed. ALT + click this icon to unsppress.");
-    else if (tag == _IconTag) {
-        if (isSetTipIcon)
-            return QObject::tr("This is the tip of the body. New feature will be inserted after it.");
-        else
-            return QObject::tr("Alt + click this icon to set this feature as the tip of the body.\n"
-                               "New feature will be inserted after it.");
+    if (!Gui::isTreeViewDragging()) {
+        if (tag == _SuppressedTag)
+            return QObject::tr("Feature suppressed. ALT + click this icon to unsppress.");
+        else if (tag == _IconTag) {
+            if (isSetTipIcon)
+                return QObject::tr("This is the tip of the body. New feature will be inserted after it.");
+            else
+                return QObject::tr("Alt + click this icon to set this feature as the tip of the body.\n"
+                                "New feature will be inserted after it.");
+        }
     }
     return inherited::getToolTip(tag);
 }
